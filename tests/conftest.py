aa834d4977071a5dd150fab7c98fcaf851c0a490
Hansen <hansen@sidratresearch.com>
2024-05-01 16:05:07 -0400

    WIP added config tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,4 +4,31 @@
 # Imports
 
 
+from pathlib import Path
 import pytest
+
+from galwrap import config
+
+
+# Fixtures
+
+
+## Path
+
+
+@pytest.fixture
+def test_root() -> Path:
+    return (Path(__file__).parent).resolve()
+
+
+@pytest.fixture
+def test_data_path(test_root: Path) -> Path:
+    return test_root / "test-data"
+
+
+## Object
+
+
+@pytest.fixture
+def galwrap_config(test_data_path: Path):
+    return config.create_config(test_data_path / "config.yaml")
