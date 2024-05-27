6ee391738d93d5fe6985c30b7086d099f9b7331b
Hansen <hansen@sidratresearch.com>
2024-05-27 04:45:26 -0400

    added logging and normalization to plots

--- src/morphfits/utils/plots.py
+++ src/morphfits/utils/plots.py
@@ -15,7 +15,7 @@ from matplotlib import pyplot as plt
 # Constants
 
 
-logger = logging.getLogger("PLOT")
+logger = logging.getLogger("PLOTS")
 """Logger object for processes from this module.
 """
 
@@ -29,9 +29,16 @@ logging.getLogger("PIL").setLevel(100)
 
 
 def plot_comparison(stamp_path: Path, model_path: Path, output_path: Path):
+    logger.info(f"Plotting model comparisons for {model_path.name}.")
+
     # Load in data
     stamp = fits.open(stamp_path)["PRIMARY"].data
-    model = fits.open(model_path)["PRIMARY"].data
+    model = fits.open(model_path)[2].data
+    norm_model = np.copy(model)
+    norm_model -= np.min(model)
+    norm_model /= np.max(model)
+    norm_model *= np.max(stamp) - np.min(stamp)
+    norm_model += np.min(stamp)
 
     # Plot data
     plt.subplots(1, 3, figsize=(20, 6))
@@ -43,12 +50,12 @@ def plot_comparison(stamp_path: Path, model_path: Path, output_path: Path):
     plt.title("Original")
 
     plt.subplot(1, 3, 2)
-    plt.imshow(model, cmap="magma")
+    plt.imshow(norm_model, cmap="magma")
     plt.axis("off")
     plt.title("Model")
 
     plt.subplot(1, 3, 3)
-    plt.imshow(stamp - model, cmap="magma")
+    plt.imshow(norm_model - stamp, cmap="magma")
     plt.axis("off")
     plt.title("Residuals")
 
