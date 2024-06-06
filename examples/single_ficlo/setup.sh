d9826ae85919768345aabaa56a97431818056f0a
Hansen <hansen@sidratresearch.com>
2024-06-06 10:36:20 -0400

    updated quickstart README and setup script

--- examples/single_ficlo/setup.sh
+++ examples/single_ficlo/setup.sh
@@ -10,17 +10,17 @@ mkdir -p psfs abell2744clu/grizli-v7.2/f200w
 # Download files
 echo Downloading files.
 cd abell2744clu/grizli-v7.2
-wget https://s3.amazonaws.com/grizli-v2/JwstMosaics/v7/abell2744clu-grizli-v7.2-ir_seg.fits.gz
-wget https://s3.amazonaws.com/grizli-v2/JwstMosaics/v7/abell2744clu-grizli-v7.2-fix_phot_apcorr.fits
+wget -q --show-progress https://s3.amazonaws.com/grizli-v2/JwstMosaics/v7/abell2744clu-grizli-v7.2-ir_seg.fits.gz
+wget -q --show-progress https://s3.amazonaws.com/grizli-v2/JwstMosaics/v7/abell2744clu-grizli-v7.2-fix_phot_apcorr.fits
 cd f200w
-wget https://s3.amazonaws.com/grizli-v2/JwstMosaics/v7/abell2744clu-grizli-v7.2-f200w-clear_drc_exp.fits.gz
-wget https://s3.amazonaws.com/grizli-v2/JwstMosaics/v7/abell2744clu-grizli-v7.2-f200w-clear_drc_sci.fits.gz
-wget https://s3.amazonaws.com/grizli-v2/JwstMosaics/v7/abell2744clu-grizli-v7.2-f200w-clear_drc_wht.fits.gz
+wget -q --show-progress https://s3.amazonaws.com/grizli-v2/JwstMosaics/v7/abell2744clu-grizli-v7.2-f200w-clear_drc_exp.fits.gz
+wget -q --show-progress https://s3.amazonaws.com/grizli-v2/JwstMosaics/v7/abell2744clu-grizli-v7.2-f200w-clear_drc_sci.fits.gz
+wget -q --show-progress https://s3.amazonaws.com/grizli-v2/JwstMosaics/v7/abell2744clu-grizli-v7.2-f200w-clear_drc_wht.fits.gz
 
 # Uncompress files
 echo Unzipping files.
-gzip -vd *
+gzip -vd *.gz
 cd ..
-gzip -vd *
+gzip -vd *.gz
 
 echo Done.
